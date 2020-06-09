532027f11ac5768a412655d6da617abe3b66968a
Yan Santos <yan17souza@hotmail.com>
2020-06-09 15:44:07 -0300

    post spec created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_202020) do
+ActiveRecord::Schema.define(version: 2020_06_09_143647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_08_202020) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "poster"
   end
 
   create_table "posts", force: :cascade do |t|
