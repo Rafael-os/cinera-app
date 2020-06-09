2e69e13bfd7fe2ba85f770faebd8f7330e2a8522
caoquerque <caoquerque@gmail.com>
2020-06-09 15:52:50 -0300

    Movies added to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require 'open-uri'
+require "pry"
+
+movies = ["Scarface", "Point Break", "The Exorcist", "Rocky", "First Blood", "The Lord of the Rings", "The Godfather", "The Good, the Bad and the Ugly", "Green Street Hooligans", "Psycho" ]
+
+def create_url(movie_name)  
+  "http://www.omdbapi.com/?&t=#{movie_name}&apikey=fc405fc1"
+end
+
+movies.each do |movie_name|
+  movie_url = create_url(movie_name)
+  response = open(movie_url).read
+  movie_post = JSON.parse(response)
+  movie = Movie.new(
+    title: movie_post["Title"],
+    director: movie_post["Director"],
+    year: movie_post["Year"],
+  )
+  movie.save
+  puts "[#{movie.title}] #{movie.director} - #{movie.year}"
+end
