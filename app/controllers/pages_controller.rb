1af258130af59b0a290bf3adbf622cabc51e2f86
caoquerque <caoquerque@gmail.com>
2020-06-11 17:19:07 -0300

    Genre.sort_by para nao esquecer mais

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @posts = Post.includes(:movie)
+    @genres = Genre.all.sort_by { |genre| genre.name }
+    if params[:genre] 
+      @posts = @posts.where(movies: {genre_id: params[:genre].to_i})
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :movie
   belongs_to :user
-
+  has_many :comments, dependent: :destroy
+  
   validates :comment, presence: true
   validates :rating, presence: true, :inclusion => 1..5  
 end
