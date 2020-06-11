cda38b14ce57a0a4777c5b15a4a6b2584c4c0fe4
Yan Santos <yan17souza@hotmail.com>
2020-06-11 17:18:43 -0300

    estilo do home/signup/login/forgot kind of done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_10_212811) do
-
-
+ActiveRecord::Schema.define(version: 2020_06_10_221146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
