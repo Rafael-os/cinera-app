245cb5e8fd4276c33ca7b514bc89ab71d67c361d
Yan Santos <yan17souza@hotmail.com>
2020-06-18 14:34:39 -0300

    small changes in follow logo

--- db/schema.rb
+++ db/schema.rb
@@ -104,7 +104,6 @@ ActiveRecord::Schema.define(version: 2020_06_18_144926) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
-    t.integer "followers"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
