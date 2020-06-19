a7a3e1a96194d49a5df250a2b359ffc4ea120ed4
caoquerque <caoquerque@gmail.com>
2020-06-19 16:21:48 -0300

    Find a movie fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,10 +36,10 @@ import { autocompleteSearch } from './autocomplete';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', async () => {
+document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initSelect2();
-  await autocompleteSearch();
+  autocompleteSearch();
 });
 
 
